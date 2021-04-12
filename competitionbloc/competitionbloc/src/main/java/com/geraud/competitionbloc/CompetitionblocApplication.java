a0a0dee9acd34288d1b67aa87a49a33684be2bd4
MGeraud <geraud.mon@gmail.com>
2021-04-12 14:32:39 +0200

    Add spring reactive structure with embedded mongo db

---
+++ competitionbloc/competitionbloc/src/main/java/com/geraud/competitionbloc/CompetitionblocApplication.java
@@ -0,0 +1,13 @@
+package com.geraud.competitionbloc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetitionblocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompetitionblocApplication.class, args);
+	}
+
+}
---
+++ competitionbloc/competitionbloc/src/test/java/com/geraud/competitionbloc/CompetitionblocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geraud.competitionbloc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompetitionblocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
