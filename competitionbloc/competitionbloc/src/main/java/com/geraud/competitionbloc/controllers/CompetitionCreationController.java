77615cee85391e1e7182478a7281e944b229a1ab
MGeraud <geraud.mon@gmail.com>
2021-04-24 15:27:45 +0200

    Add postmapping to create new competition

--- competitionbloc/competitionbloc/src/main/java/com/geraud/competitionbloc/controllers/CompetitionCreationController.java
+++ competitionbloc/competitionbloc/src/main/java/com/geraud/competitionbloc/controllers/CompetitionCreationController.java
@@ -4,10 +4,12 @@ package com.geraud.competitionbloc.controllers;
 import com.geraud.competitionbloc.models.Competition;
 import com.geraud.competitionbloc.repositories.CompetitionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
 
 @RestController()
 @RequestMapping("/competition")
@@ -23,5 +25,11 @@ public class CompetitionCreationController {
     @GetMapping("/all")
     public Flux<Competition> getAllCompetitions(){
         return competitionRepository.findAll();
+    }
+
+    @PostMapping("/creation")
+    public Mono<ResponseEntity<?>> createCompetition(@RequestBody  Competition competition) {
+        return this.competitionRepository.save(competition)
+                .map(competition1 -> ResponseEntity.created(URI.create("/competition/all")).build());
     }
 }
