ea1706079d3b63b21984de19251224129b163cd5
MGeraud <geraud.mon@gmail.com>
2021-04-25 12:12:08 +0200

    Add postmapping to create categories

--- competitionbloc/competitionbloc/src/main/java/com/geraud/competitionbloc/controllers/CompetitionCreationController.java
+++ competitionbloc/competitionbloc/src/main/java/com/geraud/competitionbloc/controllers/CompetitionCreationController.java
@@ -1,15 +1,19 @@
 package com.geraud.competitionbloc.controllers;
 
 
+import com.geraud.competitionbloc.models.Category;
 import com.geraud.competitionbloc.models.Competition;
+import com.geraud.competitionbloc.repositories.CategoryRepository;
 import com.geraud.competitionbloc.repositories.CompetitionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController()
 @RequestMapping("/competition")
@@ -17,9 +21,11 @@ public class CompetitionCreationController {
 
 
     CompetitionRepository competitionRepository;
+    CategoryRepository categoryRepository;
 
-    public CompetitionCreationController(CompetitionRepository competitionRepository) {
+    public CompetitionCreationController(CompetitionRepository competitionRepository, CategoryRepository categoryRepository) {
         this.competitionRepository = competitionRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     @GetMapping("/all")
@@ -31,5 +37,16 @@ public class CompetitionCreationController {
     public Mono<ResponseEntity<?>> createCompetition(@RequestBody  Competition competition) {
         return this.competitionRepository.save(competition)
                 .map(competition1 -> ResponseEntity.created(URI.create("/competition/all")).build());
+    }
+
+    @PostMapping("/categories")
+    public Mono<ResponseEntity<?>> createCategories(@RequestBody List<Category> categories){
+        try {
+            categories.forEach(c ->categoryRepository.save(c).subscribe());
+
+        }catch (Exception e){
+            return  Mono.just(ResponseEntity.status(HttpStatus.NOT_MODIFIED).build());
+        }
+        return Mono.just(ResponseEntity.status(201).build());
     }
 }
